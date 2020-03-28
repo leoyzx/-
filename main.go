0a0278fdaf27c7128cdb8e5752da4e1fbe0fa27f
leoyzx <502298240@qq.com>
2020-03-28 18:34:27 +0800

    第四次修改

--- main.go
+++ main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 	"leoyzx/vue+golang/common"
+	"log"
+	"os"
 )
 
 func main(){
+	InitConfig()
 	db:=common.InitDb()
 	defer db.Close()
 
 	r:=gin.Default()
 	r=CollectRoute(r)
+
+	port:=viper.GetString("port")
+	if port!="" {
+		panic(r.Run(":"+port))
+	}
 	panic(r.Run())
 }
 
+func InitConfig(){
+	workDir,err:=os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	viper.New()
+	viper.AddConfigPath(workDir+"/config")
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	//viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 
 
